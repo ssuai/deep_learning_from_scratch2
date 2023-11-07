7cf31b7e6194e5637db35fdab402f0ed3e4fe924
yyoo@aurora <yyoo@aurora>
2023-11-08 00:32:20 +0900

    English labels due to font crash

--- common/trainer.py
+++ common/trainer.py
@@ -61,8 +61,10 @@ class Trainer:
         if ylim is not None:
             plt.ylim(*ylim)
         plt.plot(x, self.loss_list, label='train')
-        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
-        plt.ylabel('손실')
+        plt.xlabel('Iteration (x' + str(self.eval_interval) + ')')
+        #plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('Loss')
+        #plt.ylabel('손실')
         plt.show()
 
 
